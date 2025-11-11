f88901e1fd5eb60e2df07475d855573782b384b6
Sandbox user <tools@notanumber.com>
2025-11-11 01:00:44 +0000

    Popravio import putanju za dataStore u reopen route API-ju

--- web-admin/app/api/sync/tickets/[id]/reopen/route.ts
+++ web-admin/app/api/sync/tickets/[id]/reopen/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import dataStore from "../../../../../lib/dataStore";
+import dataStore from "../../../../../../lib/dataStore";
 
 export async function POST(
   request: Request,
